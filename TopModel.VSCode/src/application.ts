881e115f5d753829a588d91543d5568dca969f72
Damien <damien.frikha@kleegroup.com>
2024-12-05 14:11:55 +0100

    modgen 2.2/vscode 2.2

--- TopModel.VSCode/src/application.ts
+++ TopModel.VSCode/src/application.ts
@@ -56,21 +56,7 @@ export class Application {
     }
 
     private async startLanguageServer() {
-        const stdout = await execute("dotnet --list-runtimes");
-        const runtimeVersion = stdout.includes("Microsoft.NETCore.App 8.0")
-            ? "net8.0"
-            : stdout.includes("Microsoft.NETCore.App 9.0")
-            ? "net9.0"
-            : undefined;
-
-        if (!runtimeVersion) {
-            window.showErrorMessage("Aucun runtime .NET trouvé pour lancer l'extension TopModel (net8.0 ou net9.0)");
-            return;
-        }
-
-        const args = [
-            this.extensionContext.asAbsolutePath(`./language-server-${runtimeVersion}/TopModel.LanguageServer.dll`),
-        ];
+        const args = [this.extensionContext.asAbsolutePath(`./language-server/TopModel.LanguageServer.dll`)];
         let configRelativePath = workspace.asRelativePath(this._configPath);
         args.push(this._configPath.substring(1));
         let serverOptions: ServerOptions = {
